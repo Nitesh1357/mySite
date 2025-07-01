8c7780c3e381dd6dc3d35becf324b822f482b9e9
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-07-01 11:23:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -92,6 +92,18 @@ function opentab(tabname, event) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname, event){
+    for(let tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(let tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-links");
+    document.getElementById(tabname).classList.add("active-tab");
+}
 
 // var tablinks = document.getElementsByClassName("tab-links");
 // var tabcontents = document.getElementsByClassName("tab-contents");
